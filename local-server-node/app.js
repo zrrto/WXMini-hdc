d5f823a651e0c6a625e5ef589fa876fe6e36836c
zrrto <2516122612@qq.com>
2020-11-28 16:22:48 +0800

    update local server

--- local-server-node/app.js
+++ local-server-node/app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path')
 var app = express()
+var fs = require("fs")
 
 app.get('/', (req, res)=>{
     res.send('Hello world');
@@ -15,6 +16,13 @@ app.get('/report', function (req, res, next) {
 })
 app.get('/air', function (req, res, next) {
     res.sendFile(path.join(__dirname, 'public/lenveo.mp4'));
+})
+
+app.get('/files',function (req, res) {
+   var path1 = path.join(__dirname, 'public/Files.json');
+   var data = fs.readFileSync(path1);
+   var response = JSON.parse(data);
+   res.end(JSON.stringify(response));
 })
 app.listen(7000, ()=>{
     console.log('Server is running at http://localhost:7000')
